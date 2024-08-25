8156c04ec6df617a4b16a6ad05383bcc1263ea9a
Julie Fiess <juliefiess@gmail.com>
2024-08-25 10:52:12 -0500

    Submission for quiz

--- src/NumberGenerator.java
+++ src/NumberGenerator.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.function.Supplier;
 
 /**
@@ -23,8 +24,10 @@ public class NumberGenerator {
      * @return a new list of 5 random integers, where 0 <= value <= 499.
      */
     public List<Integer> generateNumbers() {
+        Random random = new Random();
         // TODO: replace second argument with a lambda expression that implements Supplier<Integer>
-        return populateNumbers(5, null);
+        return populateNumbers(5, () -> random.nextInt(500));
+        //return populateNumbers(5, null);
     }
 
     private List<Integer> populateNumbers(int size, Supplier<Integer> numberGenerator) {
@@ -32,7 +35,8 @@ public class NumberGenerator {
 
         for (int i = 0; i < size; i++) {
             // TODO: Replace argument to add() with a call to numberGenerator's method
-            numbers.add(-1);
+            numbers.add(numberGenerator.get());
+            //numbers.add(-1);
         }
 
         return numbers;
